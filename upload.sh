3708607a138052b7584c3264504d2901d6555afb
overoptimus <1040570917@qq.com>
2020-04-29 21:23:58 +0800

    修改node版本

--- upload.sh
+++ upload.sh
@@ -1,7 +1,12 @@
+###
+ # @Description: 
+ # @Version: 2.0
+ # @Author: 0pt1mus
+ # @Date: 2019-11-22 00:36:50
+ # @LastEditors: 0pt1mus
+ # @LastEditTime: 2020-04-29 20:47:15
+ ###
 hexo clean
 hexo g
+echo "superj.ste" > ./public/CNAME
 hexo d
-cp -r /Users/optimus/myblog/source /Users/optimus/Library/Mobile\ Documents/com\~apple\~CloudDocs/博客备份/资源备份/
- cp _config.yml /Users/optimus/Library/Mobile\ Documents/com\~apple\~CloudDocs/博客备份/站点备份
-cp /Users/optimus/myblog/themes/next/_config.yml /Users/optimus/Library/Mobile\ Documents/com\~apple\~CloudDocs/博客备份/主题备份/next/
-cp /Users/optimus/myblog/themes/pure/_config.yml /Users/optimus/Library/Mobile\ Documents/com\~apple\~CloudDocs/博客备份/主题备份/pure/
